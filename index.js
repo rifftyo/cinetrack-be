ff9c746e1df925d9694511096137ea2529784a40
RIFKY TYO <rifftyo@gmail.com>
2026-01-22 07:35:16 +0700

    updata fix

--- index.js
+++ index.js
@@ -1,41 +1,21 @@
 require('dotenv').config();
 
 const express = require('express');
-const cors = require('cors');
-
 const app = express();
-
-// ENABLE CORS → PENTING UNTUK RAILWAY (POST/PUT/DELETE)
-app.use(
-  cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  }),
-);
-
-// FIX BODY PARSER
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// ROUTES
 const authRoutes = require('./routes/authRoutes');
 const movieRoutes = require('./routes/movieRoutes');
 const watchedRoutes = require('./routes/watchedRoutes');
 const statisticsRoutes = require('./routes/statisticsRoutes');
 const userRoutes = require('./routes/userRoutes');
 
+app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/movies', movieRoutes);
 app.use('/api/watched', watchedRoutes);
 app.use('/api/statistics', statisticsRoutes);
 app.use('/api/user', userRoutes);
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server berjalan di port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
